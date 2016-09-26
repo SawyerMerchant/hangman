b8fe80966979837a0640227b42d7a624eb205f05
Sawyer <sawyer@sawyermerchant.com>
2016-09-26 14:49:24 +0000

    Allow user to pick level, display empty spaces & wrong guesses, and allow user to guess

--- write.rb
+++ write.rb
@@ -1,5 +1,49 @@
+require 'sinatra'
+require 'sinatra/reloader'
 require 'yaml'
 
-def write()
-  YAML.load_file('library.yml')[2].sample
+@@secret_word = ""
+@@misses = []
+@@correct_guesses = []
+@@miss_count = 5
+@@message = ""
+@@level = 0
+def reset(level)
+  level = level.to_i
+  @@secret_word = YAML.load_file('../hang_man/library.yml')[level].sample
+  # params.delete :guess
+  @@misses = []
+  @@correct_guesses = []
+  @@message = ""
+  @@miss_count = 5
+end
+
+def make_guess(guess)
+  guess = guess.downcase
+  return @@message = "Careful, your guess must be a single letter" if !('a'..'z').include?(guess)
+  if !@@secret_word.nil? && !guess.nil?
+    if @@secret_word.include?(guess)
+      # add that character to misses
+      @@correct_guesses << guess if !@@correct_guesses.include?(guess)
+      @@message = "Good one!"
+    else
+      #add to misses
+      @@misses << guess if !@@misses.include?(guess)
+      @@miss_count -= 1
+      @@message = "nope, no #{guess}!"
+    end
+  end
+end
+
+get '/' do
+  reset(params[:level]) if params[:level]
+  guess = params[:guess]
+  # @@level = params[:level].to_i
+  # if ('a'..'z').include?(guess.downcase)
+  make_guess(guess) if params[:guess]
+  # else
+  #   message = "Careful, you can only guess a single letter at a time"
+  # end
+  erb :index, :locals => { :secret_word => @@secret_word, :misses => @@misses, :miss_count => @@miss_count, :correct_guesses => @@correct_guesses, :message => @@message, :level => @@level }
+  # throw params.inspect
 end
